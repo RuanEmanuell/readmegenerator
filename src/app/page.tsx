9d3f2cb6ddf09c75648ae67f52f82bbd7162a3ec
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2024-04-04 12:40:49 -0300

    (Version 1.0) Adding techs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,49 +13,106 @@ export default function Home() {
   const [selectedTechs, setSelectedTechs] = useState<Tech[]>([]);
 
   const techs: Tech[] = [
+    // Web
     ["HTML", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"],
     ["CSS", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"],
     ["Javascript", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"],
-  ]
+    ["React", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"],
+    ["Angular", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg"],
+    ["Vue.js", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg"],
+    ["Node.js", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"],
+    ["Express.js", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg"],
+    ["Python", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"],
+    ["Ruby", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ruby/ruby-original.svg"],
+    ["ASP.NET", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dot-net/dot-net-original.svg"],
+    ["Java", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"],
+    ["Spring", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original.svg"],
+  
+    // Mobile
+    ["Flutter", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flutter/flutter-original.svg"],
+    ["React Native", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"],
+    ["Ionic", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ionic/ionic-original.svg"],
+    ["Swift", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swift/swift-original.svg"],
+    ["Kotlin", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/kotlin/kotlin-original.svg"],
+    ["Xamarin", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/xamarin/xamarin-original.svg"],
+  
+    // Desktop
+    ["Electron", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/electron/electron-original.svg"],
+    ["C#", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg"],
+    ["Qt", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/qt/qt-original.svg"],
+  
+    // Databases
+    ["PostgreSQL", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg"],
+    ["MongoDB", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"],
+    ["MySQL", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"],
+    ["Redis", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redis/redis-original.svg"],
+  
+    // Testing
+    ["Jest", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-original.svg"],
+    ["Mocha", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mocha/mocha-original.svg"],
+    ["Cypressio", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cypress/cypressio-original.svg"],
+  
+    // CI/CD & DevOps
+    ["Docker", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg"],
+    ["Kubernetes", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/kubernetes/kubernetes-original.svg"],
+    ["Git", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"],
+    ["GitHub", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"],
+    ["GitHub Actions", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"],
+    ["GitLab", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original.svg"],
+  
+    // Scripting
+    ["Bash", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bash/bash-original.svg"],
+    ["PowerShell", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/powershell/powershell-original.svg"],
+  
+    // Games
+    ["Unity", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/unity/unity-original.svg"],
+    ["Unreal Engine", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/unreal/unreal-original.svg"],
+    ["Godot", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/godot/godot-original.svg"]
+  ];
+  
 
   function addTech() {
-    if(!selectedTechs.some(tech => JSON.stringify(tech) === JSON.stringify(currentTech))){
-    setSelectedTechs(prev => [...prev, currentTech]);
+    if (!selectedTechs.some(tech => JSON.stringify(tech) === JSON.stringify(currentTech))) {
+      setSelectedTechs(prev => [...prev, currentTech]);
     }
   }
 
+  function removeTech(tech: Tech) {
+    setSelectedTechs(prev => prev.filter(item => item !== tech));
+  }
+
   return (
-    <div className="w-full h-full">
-      <main className="max-w-screen-xl h-max m-auto flex justify-center flex-col">
+    <div className="w-full h-max">
+      <main className="max-w-screen-xl h-full m-auto flex justify-center flex-col">
         <h1 className="text-center text-white font-bold text-4xl my-5">README Generator</h1>
-        <section className="w-11/12 h-full mx-auto border-4 border-green-700 rounded">
-          <div className="w-full md:h-1/6 sm:h-1/3 grid md:grid-cols-2 sm:grid-cols-1">
+        <section className="w-11/12 md:w-10/12 h-full mx-auto border-4 border-green-700 rounded">
+          <div className="w-full h-1/3 md:h-1/6 grid grid-cols-1 md:grid-cols-2">
             <div className="h-full flex justify-center flex-col">
               <h1 className="text-white my-5 text-center">Project Name</h1>
-              <input className="whiteBox h-1/2 w-11/12"
+              <input className="whiteBox h-1/2 w-11/12 m-auto"
                 value={projectName}
                 onChange={(event) => setProjectName(event.target.value)}>
               </input>
             </div>
             <div className="flex justify-center flex-col">
               <h1 className="text-white my-5 text-center">Project Website</h1>
-              <input className="whiteBox h-1/2 w-11/12"
+              <input className="whiteBox h-1/2 w-11/12 m-auto"
                 value={projectWebsite}
                 onChange={(event) => setProjectWebsite(event.target.value)}>
               </input>
             </div>
           </div>
-          <div className="w-full h-1/3 grid md:grid-cols-2 sm:grid-cols-1">
+          <div className="w-full h-1/3 grid grid-cols-1 md:grid-cols-2">
             <div className="h-full w-full flex justify-center flex-col">
               <h1 className="text-white my-5 text-center">Project Description</h1>
-              <textarea className="whiteBox h-3/4 w-11/12"
+              <textarea className="whiteBox h-3/4 w-11/12 m-auto"
                 value={projectDescription}
                 onChange={(event) => setProjectDescription(event.target.value)}>
               </textarea>
             </div>
             <div className="h-full w-full flex justify-center flex-col">
               <h1 className="text-white my-5 text-center">Project Run Instructions</h1>
-              <textarea className="whiteBox h-3/4 w-11/12"
+              <textarea className="whiteBox h-3/4 w-11/12 m-auto"
                 value={projectInstructions}
                 onChange={(event) => setProjectInstructions(event.target.value)}>
               </textarea>
@@ -64,9 +121,9 @@ export default function Home() {
           <div className="w-full h-1/2">
             <div className="h-full w-full text-center">
               <h1 className="text-white my-5">Project Techs</h1>
-              <section className="whiteBox h-5/6 w-11/12 flex flex-col">
-                <select className="bg-gray-800 text-white w-64 h-16 text-center my-4 m-auto">
-                  {Object.values(techs).map(tech =>
+              <section className="whiteBox h-5/6 w-11/12 flex flex-col my-5 mx-auto">
+                <select className="bg-gray-800 text-white w-1/2 h-16 text-center my-4 m-auto">
+                  {Object.values(techs.sort()).map(tech =>
                     <option
                       className="bg-gray-800"
                       key={tech[0]}
@@ -75,14 +132,14 @@ export default function Home() {
                 </select>
                 <button className="bg-green-800 text-white w-32 h-12 font-bold mx-auto"
                   onClick={addTech}>Add</button>
-                <div className="h-1/2 grid md:grid-cols-6 sm:grid-cols-3 my-5">
-                    {Object.values(selectedTechs).map(tech =>
-                      <div key={tech[0]} className="w-32 h-10 mx-5 grid grid-cols-3 bg-gray-900">
-                        <div className="col-span-1 h-6 my-auto">
+                <div className="h-1/2 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 my-5">
+                  {Object.values(selectedTechs).map(tech =>
+                    <div key={tech[0]} className="w-40 h-10 mx-auto my-2.5 grid grid-cols-5 bg-gray-800 cursor-pointer" onClick={() => removeTech(tech)}>
+                      <div className="col-span-1 my-auto">
                         <img src={tech[1]} alt={tech[0]} className="h-auto w-1/2 object-cover mx-2.5" />
-                        </div>
-                        <p className="col-span-2 text-white my-auto">{tech[0]}</p>
-                      </div>)}
+                      </div>
+                      <p className="col-span-4 text-white my-auto">{tech[0]}</p>
+                    </div>)}
                 </div>
               </section>
             </div>
